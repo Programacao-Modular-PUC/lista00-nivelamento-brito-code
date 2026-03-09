975b38e58ba7840b176303a60f36859a85f5f6a2
brito-code <vini.brito.dellisola@gmail.com>
2026-03-09 20:02:37 -0300

    Interseção de vetores
    Added functionality to read student enrollments for Programming and Modeling courses, and display students enrolled in both.

--- Lista00.java
+++ Lista00.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class Lista00 {
     public static void main(String[] args) {
-        System.out.println("Minha lista de nivelamento");
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Digite a quantidade de alunos (n):");
+        int n = leitor.nextInt();
+
+        int[] prog = new int[n];
+        int[] model = new int[n];
+        System.out.println("Digite as matriculas de Programacao:");
+        for (int i = 0; i < n; i++) {
+            prog[i] = leitor.nextInt();
+        }
+
+        System.out.println("Digite as matriculas de Modelagem:");
+        for (int i = 0; i < n; i++) {
+            model[i] = leitor.nextInt();
+        }
+
+        System.out.println("Alunos matriculados em ambas as UCs (Interseção):");
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (prog[i] == model[j]) {
+                    System.out.println("Matricula: " + prog[i]);
+                    break;
+                }
+            }
+        }
+
+        leitor.close();
     }
 }
