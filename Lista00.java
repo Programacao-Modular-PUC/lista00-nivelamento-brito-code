b1ee74b81bb52b0b422e89a117942dbddf9032be
brito-code <vini.brito.dellisola@gmail.com>
2026-03-09 17:39:25 -0300

    Calculo de fatorial em Java

--- Lista00.java
+++ Lista00.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Lista00 {
     public static void main(String[] args) {
-        System.out.println("Minha lista de nivelamento");
+        Scanner leitor = new Scanner(System.in);
+        
+        System.out.println("Digite o número para encontrar o valor do seu fatorial: ");
+        int x = leitor.nextInt();
+        double fatorial = 1; 
+
+        for(int i = 1; i <= x; i++) {
+            fatorial *= i;
+        }
+
+        System.out.println("O fatorial de " + x + " é: " + fatorial);
+        leitor.close();
     }
 }
