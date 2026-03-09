df81cad47954666d27f1d37793b60177cacfe301
brito-code <vini.brito.dellisola@gmail.com>
2026-03-09 20:12:32 -0300

    Variação de temperatura

--- Lista00.java
+++ Lista00.java
@@ -1,5 +1,41 @@
+import java.util.Scanner;
+
 public class Lista00 {
     public static void main(String[] args) {
-        System.out.println("Minha lista de nivelamento");
+        Scanner leitor = new Scanner(System.in);
+        
+        double[] temperaturas = new double[12];
+        String[] meses = {
+            "Janeiro", "Fevereiro", "Marco", "Abril", "Maio", "Junho",
+            "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"
+        };
+
+        for (int i = 0; i < 12; i++) {
+            System.out.print("Digite a temperatura media de " + meses[i] + ": ");
+            temperaturas[i] = leitor.nextDouble();
+        }
+
+        double maiorTemp = temperaturas[0];
+        int indiceMaior = 0;
+        
+        double menorTemp = temperaturas[0];
+        int indiceMenor = 0;
+
+        for (int i = 1; i < 12; i++) {
+            if (temperaturas[i] > maiorTemp) {
+                maiorTemp = temperaturas[i];
+                indiceMaior = i;
+            }
+            if (temperaturas[i] < menorTemp) {
+                menorTemp = temperaturas[i];
+                indiceMenor = i;
+            }
+        }
+
+        System.out.println("\n--- RELATORIO CLIMATICO ANUAL ---");
+        System.out.println("Maior temperatura: " + maiorTemp + " (" + meses[indiceMaior] + ")");
+        System.out.println("Menor temperatura: " + menorTemp + " (" + meses[indiceMenor] + ")");
+
+        leitor.close();
     }
 }
